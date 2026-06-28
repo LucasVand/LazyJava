5c5511580243ed44139437a633065c547432d144
Lucas <lucasvanderwielen@icloud.com>
2026-06-28 11:47:42 -0400

    removed test proj

--- test_filesystem/test2/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-/* Created with LazyJava */ 
-public class Main {
-        
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Welcome to your LazyJava project");
-    }
-}
