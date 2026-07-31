63d0aff6393ce39bad929806aa43730f18aec06e
Lucas <lucasvanderwielen@icloud.com>
2026-07-30 22:12:26 -0400

    testing

--- src/build/metadata.rs
+++ src/build/metadata.rs
@@ -102,7 +102,7 @@ pub fn hash_directory(path: &Path) -> u64 {
             if let Ok(modified) = meta.modified() {
                 modified.hash(&mut hasher);
                 let dur = modified.duration_since(UNIX_EPOCH).unwrap_or_default();
-                log::trace!(
+                log::debug!(
                     "hash_dir {:?} size={} mod={}.{:09}",
                     dir.path(),
                     meta.len(),
